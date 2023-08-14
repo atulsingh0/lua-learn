0913acc88b4df4478dff2812fb88fad61d428a51
atulsingh0 <3430610+atulsingh0@users.noreply.github.com>
2023-08-13 23:55:39 -0400

    main | func n user input

---
+++ function.lua
@@ -0,0 +1,12 @@
+local function hi ()
+    print("Hello")
+end
+
+hi()
+hi()
+
+local function add (a, b)
+    return a+b
+end
+
+print(add(3, 2))
---
+++ user-input.lua
@@ -0,0 +1,6 @@
+
+print("What is your name?")
+local name = io.read();
+
+print("name:", name, "!")
+print("My name is "..name..".")
