7a070a466ddae48550f288d1000d141e1e7c2f99
atulsingh0 <3430610+atulsingh0@users.noreply.github.com>
2023-08-14 22:59:06 -0400

    main | comments n print stmt

---
+++ comments.lua
@@ -0,0 +1,15 @@
+-- this is one line comments
+
+
+-- there is no space b/w -- and [[
+--[[ This is multi line 
+ comment block which 
+ you can to add comments
+--]]
+
+
+print("this is single line print statement")
+
+print([["this is multi
+line print 
+statement."]])
---
+++ prints.lua
@@ -0,0 +1,6 @@
+local name="Atul"
+print("Hi, my name is "..name)
+
+print("We can write the same string as this", name)
+
+print("But above stmt will add one extra space in between.")
